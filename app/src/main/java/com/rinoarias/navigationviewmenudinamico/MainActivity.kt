1066134660ef0b352e2abcca27d6f8dc9ab0d935
Rino Arias <rinoariaslaaz@gmail.com>
2022-08-20 23:37:05 -0500

    Conexión con API + validación

--- app/src/main/java/com/rinoarias/navigationviewmenudinamico/MainActivity.kt
+++ app/src/main/java/com/rinoarias/navigationviewmenudinamico/MainActivity.kt
@@ -1,28 +1,85 @@
 package com.rinoarias.navigationviewmenudinamico
 
+import android.content.Intent
 import android.os.Bundle
-import android.view.Menu
 import android.view.View
-import com.google.android.material.snackbar.Snackbar
-import com.google.android.material.navigation.NavigationView
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
-import androidx.drawerlayout.widget.DrawerLayout
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.rinoarias.navigationviewmenudinamico.databinding.ActivityMainBinding
+import com.android.volley.Request
+import com.android.volley.RequestQueue
+import com.android.volley.toolbox.JsonArrayRequest
+import com.android.volley.toolbox.Volley
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var btnLogin: Button
+    lateinit var txtUsername: EditText
+    lateinit var txtPassword: EditText
+    lateinit var requestQueue: RequestQueue
+    val url = "https://my-json-server.typicode.com/rinoarias/NavigationViewMenuDinamico/Usuarios"
+    var userList: ArrayList<Usuario> = ArrayList<Usuario>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnLogin = findViewById(R.id.btnLogin)
+        txtUsername = findViewById(R.id.txtUsername)
+        txtPassword = findViewById(R.id.txtPassword)
+
+        obtenerUsuarios()
+
     }
 
-    fun iniciarSesion(view : View) {
+    private fun obtenerUsuarios() {
+        var requestJson : JsonArrayRequest = JsonArrayRequest(
+            Request.Method.GET,
+            url,
+            null,
+            { response ->
+                try {
+                    for (i in 0 until response.length()){
+                        var item = response.getJSONObject(i)
+                        userList.add(Usuario(item))
+                    }
+                } catch (e: Exception) {
+                    Toast.makeText(this, "Error al obtener los datos: $e", Toast.LENGTH_LONG).show()
+                    System.out.println(e.toString())
+                }
+            },
+            {
+                Toast.makeText(this, "Error al obtener los datos: $it", Toast.LENGTH_LONG).show()
+                System.out.println(it.toString())
+            })
+        requestQueue = Volley.newRequestQueue(this)
+        requestQueue.add(requestJson)
+    }
 
+    fun iniciarSesion(view : View?) {
+        var userPosition = validarUsuario(txtUsername.text.toString(), txtPassword.text.toString())
+        if(userPosition >= 0) {
+            val intent : Intent = Intent(this, NavigationActivity::class.java)
+            intent.putExtra("usuario", userList.get(userPosition))
+            startActivity(intent)
+        } else {
+            Toast.makeText(this, "Usuario y/o contraseña incorrectos.", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    private fun validarUsuario(usuario: String, password: String) : Int {
+        try {
+            for (i in 0 until userList.size) {
+                var auxUsuario = userList.get(i)
+                if(usuario == auxUsuario.username && password == auxUsuario.password){
+                    return i
+                }
+            }
+        } catch (e: Exception) {
+            Toast.makeText(this, "Error al validar usuario.", Toast.LENGTH_LONG).show()
+        }
+        return -1
     }
 
 }
--- app/src/main/java/com/rinoarias/navigationviewmenudinamico/Usuario.kt
+++ app/src/main/java/com/rinoarias/navigationviewmenudinamico/Usuario.kt
@@ -1,11 +1,40 @@
 package com.rinoarias.navigationviewmenudinamico
 
-import android.os.Parcel
-import android.os.Parcelable
-import android.os.Parcelable.Creator
 import org.json.JSONArray
 import org.json.JSONException
+import org.json.JSONObject
+import java.io.Serializable
 
-class Usuario  {
+class Usuario (a: JSONObject) : Serializable {
+    var username : String
+    var password : String
+    var names : String
+    var img : String
+    var opciones : ArrayList<String>
+    var rol : String
 
+
+    companion object {
+        @Throws(JSONException::class)
+        fun JsonObjectsBuild(datos: JSONArray): ArrayList<Usuario> {
+            val users : ArrayList<Usuario> =  ArrayList<Usuario>()
+            for(i in 0 until datos.length()) {
+                users.add(Usuario(datos.getJSONObject(i)))
+            }
+            return users
+        }
+    }
+
+    init {
+        username = a.getString("username").toString()
+        password = a.getString("password").toString()
+        names = a.getString("names").toString()
+        img = a.getString("img").toString()
+        opciones = ArrayList<String>()
+        val arrayOptions = a.getJSONArray("opciones")
+        for (i in 0 until arrayOptions.length()){
+            opciones.add(arrayOptions.getJSONObject(i).getString("opt"))
+        }
+        rol = a.getString("rol").toString()
+    }
 }
