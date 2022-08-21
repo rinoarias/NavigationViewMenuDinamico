6e8d34262b4557df3309121d86df4ab6fdec4c9b
Rino Arias <rinoariaslaaz@gmail.com>
2022-08-20 21:37:26 -0500

    2do commit

--- app/src/main/java/com/rinoarias/navigationviewmenudinamico/MainActivity.kt
+++ app/src/main/java/com/rinoarias/navigationviewmenudinamico/MainActivity.kt
@@ -2,6 +2,7 @@ package com.rinoarias.navigationviewmenudinamico
 
 import android.os.Bundle
 import android.view.Menu
+import android.view.View
 import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.navigation.NavigationView
 import androidx.navigation.findNavController
@@ -15,43 +16,13 @@ import com.rinoarias.navigationviewmenudinamico.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var appBarConfiguration: AppBarConfiguration
-    private lateinit var binding: ActivityMainBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        setSupportActionBar(binding.appBarMain.toolbar)
-
-        binding.appBarMain.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
-        val drawerLayout: DrawerLayout = binding.drawerLayout
-        val navView: NavigationView = binding.navView
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow
-            ), drawerLayout
-        )
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
+        setContentView(R.layout.activity_main)
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.main, menu)
-        return true
+    fun iniciarSesion(view : View) {
+
     }
 
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
-    }
 }
---
+++ app/src/main/java/com/rinoarias/navigationviewmenudinamico/Usuario.kt
@@ -0,0 +1,11 @@
+package com.rinoarias.navigationviewmenudinamico
+
+import android.os.Parcel
+import android.os.Parcelable
+import android.os.Parcelable.Creator
+import org.json.JSONArray
+import org.json.JSONException
+
+class Usuario  {
+
+}
